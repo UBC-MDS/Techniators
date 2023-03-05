f5e2aaa59bc78ccf56285f60e2ef7680acd37a39
Caesar Wong <caesar.wongw@gmail.com>
2023-03-05 14:00:12 -0800

    add temp function, images

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,17 +6,13 @@ import seaborn as sns
 import numpy as np
 import boto
 import altair as alt
+from wordcloud import WordCloud
+from PIL import Image
 
-st.title('Techniators 💻 Fake News Detector')
-
-st.markdown("""
-This app receives client input and detect whether the news is fake or not.
-* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
-* **Data source:** [Kaggle - Fake and Real News Dataset](https://www.kaggle.com/datasets/clmentbisaillon/fake-and-real-news-dataset).
-""")
-
-st.sidebar.header('User Input Features')
-
+# ==============================
+# Main functions
+# ==============================
+@st.cache
 def load_data():
     df = pd.read_csv('https://media.githubusercontent.com/media/caesarw0/news-dataset/main/data/raw_data/True.csv')
     return df
@@ -26,21 +22,69 @@ def data_preprocessing(text):
     preprocessed_text = text
     return preprocessed_text
 
-def read_model():
+def read_model(url):
+
 
     return 0
 
-def create_visualization():
+def create_wordCloud(text):
     # word cloud
 
-    # freq
+    return plt
 
-    return 0
+def create_distribution(text):
+    # frequency plot
+
+    return plt
+
 
 def model_predict():
     return 0
 
-df = load_data()
 
-st.dataframe(df.head())
+# ==============================
+# main page
+# ==============================
+st.set_page_config(page_title="Techniator - Fake News Detector",
+                   page_icon="img/sticker_icon.png")
 
+# Display the image in the middle of the page
+image = Image.open("img/sticker_icon.png")
+st.title('Techniators 💻 Fake News Detector')
+
+st.markdown("""
+This app receives client input and detect whether the news is fake or not.
+* **Python libraries:** base64, pandas, streamlit, numpy, matplotlib, seaborn
+* **Data source:** [Kaggle - Fake and Real News Dataset](https://www.kaggle.com/datasets/clmentbisaillon/fake-and-real-news-dataset).
+""")
+
+
+
+# ==============================
+# sidebar settings
+# ==============================
+st.sidebar.image(
+    image,
+    caption="Techniator - Fake News Detector",
+    use_column_width=True,
+    output_format="JPEG" 
+)
+st.sidebar.title("Input News Article")
+# input text area
+max_chars = 6000  # limit text input char
+input_height = 300  # height of the text area
+text_input = st.sidebar.text_area("Enter News Article (max {} characters):".format(max_chars), max_chars=max_chars, height=input_height)
+
+# ==============================
+# submission trigger
+# ==============================
+if st.sidebar.button("Submit"):
+    st.write("Input Text:")
+    st.write(text_input)
+
+    processed_text = data_preprocessing(text_input)
+
+    
+    # st.write("Word Cloud:")
+    # wc_plot = create_visualization(text_input)
+    # st.pyplot(wc_plot)
